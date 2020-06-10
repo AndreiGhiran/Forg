4c14ed91ba7851352022e96fe778ef61217463a6
AndreiG <48528839+AndreiGhiran@users.noreply.github.com>
2020-06-10 22:04:38 +0300

    better shoping list

--- shoping_list.php
+++ shoping_list.php
@@ -2,10 +2,32 @@
 session_start();
 if(!isset($_SESSION['email']))
 {
-	 echo "<script>location.href = 'login.php'</script>";
+     echo "<script>location.href = 'login.php'</script>";
+     
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    include('Includere/connection.php');
+    
+    
+    
+            $stmt = $dbh->prepare("DELETE FROM shoping_lists WHERE user_id=:id AND product_id = :pr_id;");
+
+            $stmt->bindParam(':id', $u_id);
+            $stmt->bindParam(':pr_id', $p_id);
+
+            $u_id = $_POST["user"];
+            $p_id = $_POST["id"];
+
+            $stmt->execute();
+    
+    $dbh = null;
+    // echo "<script>location.href = 'shoping_list.php'</script>";
 }
 ?>
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,6 +35,42 @@ if(!isset($_SESSION['email']))
     <title>FoodOrg Statistics</title>
     <link href="stylesheets/shoping_list.css" rel="stylesheet">
 </head>
+
+<script>
+
+    function remove_item(value) {
+        console.log(value);
+        var user = value.split("_")[0];
+        var id = value.split("_")[1];
+
+        var list = document.getElementById("list");
+        var row = document.getElementById(id);
+
+        list.deleteRow(row.rowIndex);
+
+        const form = document.createElement('form');
+        form.method = "post";
+        form.action = window.location.href;
+        
+        var hiddenField = document.createElement('input');
+        hiddenField.type = 'hidden';
+        hiddenField.name = "user";
+        hiddenField.value = user;
+        form.appendChild(hiddenField);
+
+        var hiddenField = document.createElement('input');
+        hiddenField.type = 'hidden';
+        hiddenField.name = "id";
+        hiddenField.value = id;
+        form.appendChild(hiddenField);
+        
+
+        document.body.appendChild(form);
+        form.submit();
+    }
+
+</script>
+
 <body>
     <header>
         <?php
@@ -23,8 +81,7 @@ if(!isset($_SESSION['email']))
     <main>
        <div class="headlines">
             <h1>Your Sopping List</h1>
-        </div>
-        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+       
                 <?php
                 include('Includere/connection.php');
                 $stmt = $dbh->prepare("SELECT id FROM `users` where email = :email;");
@@ -46,7 +103,7 @@ if(!isset($_SESSION['email']))
                     $stmt->execute();
                     if($stmt->rowCount() > 0){
                         
-                        echo '<table style="width:100%" class = "statistics_table">
+                        echo '<table style="width:100%" class = "statistics_table" id="list">
                         <tr>
                             <th>Product</th>
                             <th>Quantiti</th>
@@ -57,7 +114,7 @@ if(!isset($_SESSION['email']))
                     while ($row = $stmt->fetch()) {
                         $items[] =  ucfirst($row['name']) . '_check';
                         if(count(explode("_", $row['name'])) > 1){
-                            echo '<tr>
+                            echo '<tr id = "'. $row['product_id'] .'">
                                 <td>';
                             $words = explode("_", $row['name']);
                             for ($i = 0; $i< count($words)-1;$i++){
@@ -65,26 +122,24 @@ if(!isset($_SESSION['email']))
                             }
                             echo ucfirst($words[count($words) - 1]) . '</td> 
                                 <td>'.$row['quantity'].'</td>
-                                <td><input type="checkbox" id="productcheck" name="' .  ucfirst($row['name']) . '_check" value= '. $row['product_id'] . ' ></td>
+                                <td><button class="main_button" value ="'. $usr_id . '_' . $row['product_id'] . '" onclick ="remove_item(this.value);" >Remove</button></td>
                             </tr>';
                         }
                         else{
-                            echo '<tr>
+                            echo '<tr id = "'. $row['product_id'] .'">
                                 <td>' .  ucfirst($row['name']) . '</td>
                                 <td>'.$row['quantity'].'</td>
-                                <td><input type="checkbox" id="productcheck" name="' .  ucfirst($row['name']) . '_check" value= '. $row['product_id'] . ' ></td>
+                                <td><button class="main_button" value ="'. $usr_id . '_' . $row['product_id'] . '" onclick ="remove_item(this.value);" >Remove</button></td>
                             </tr>';
                         }
                     }
 
                     $dbh = null;
-                    echo ' </table> 
-                
-                    <input type="submit" name="submit" class="main_button" value="Remove Checked Items">';
+                    echo ' </table> </div>';
                     
                 }
                 else{
-                    echo '</form>
+                    echo '</div>
                     <div class="headlines2">
                         <h3>Your Sopping List Appears to be Empty. <a href="search.php">Search</a> for what you want and add it to your list.</h3>
                     </div>';
@@ -92,35 +147,10 @@ if(!isset($_SESSION['email']))
                 ?>
            
 		    
-        
+           
        
     </main>
 
 </body>
 </html>
-
-<?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST")
-	{
-		include('Includere/connection.php');
-        
-        
-        for($i = 0; $i< count($items); $i++){
-            if(isset($_POST[$items[$i]])){
-                $stmt = $dbh->prepare("DELETE FROM shoping_lists WHERE user_id=:id AND product_id = :pr_id;");
-
-                $stmt->bindParam(':id', $u_id);
-                $stmt->bindParam(':pr_id', $p_id);
-
-                $u_id = $usr_id;
-                $p_id = $_POST[$items[$i]];
-
-                $stmt->execute();
-            }
-        }
-
-        $dbh = null;
-        echo "<script>location.href = 'shoping_list.php'</script>";
-	}
-?>
 
