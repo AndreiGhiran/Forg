bdade0a26de9bd0c0db298383876e41062106d4b
AndreiG <48528839+AndreiGhiran@users.noreply.github.com>
2020-06-11 04:20:39 +0300

    Update register.php
    am adaugat ban check

--- register.php
+++ register.php
@@ -86,60 +86,77 @@ if(isset($_SESSION['email']))
 		$email = $_POST["email"];
 		$password = $_POST["password"];
 		$re_password = $_POST["re_password"];
-		
-		if($password == $re_password){	
-			if(strlen($email) < 5 || strlen($password) < 5 || strlen($firstname) < 2 || strlen($lastname) < 2 || !strpos($email,'@'))
-			{
-				echo '<script type="text/javascript">alert("E-mail must be valid and must have at least 5 characters.  The password must contain at least 5 characters.")</script>';
-				echo "<script>location.href = 'register.php'</script>";
-			}
-			else
-			{
-				include('Includere/connection.php');
-				$stmt = $dbh->prepare("SELECT `email` FROM `users` WHERE `email`=:email");
-				$stmt->bindParam(':email', $email);
-				$email = $_POST["email"];
-				$stmt->execute();
-				$count = $stmt->rowCount();
-				if ($count == 0)
+
+		include('Includere/connection.php');
+
+		$stmt = $dbh->prepare("SELECT `email` FROM `banned_users` WHERE `email`=:email");
+		$stmt->bindParam(':email', $email);
+
+		$email = $_POST["email"];
+
+		$count = $stmt->rowCount();
+
+		if ($count == 0){
+			echo '<script type="text/javascript">alert("Thie email adress has been banned from our sebsite")</script>';
+			echo "<script>location.href = 'home.php'</script>";
+		}
+		else
+		{
+			if($password == $re_password){	
+				if(strlen($email) < 5 || strlen($password) < 5 || strlen($firstname) < 2 || strlen($lastname) < 2 || !strpos($email,'@'))
 				{
-					$stmt =  $dbh->prepare("INSERT INTO `users` (email, password, first_name, last_name, isAdmin) VALUES (:email, :password, :first_name, :last_name, :isAdmin)");
-
-					$stmt->bindParam(':email', $email);
-					$stmt->bindParam(':password', $password);
-					$stmt->bindParam(':first_name', $firstname);
-					$stmt->bindParam(':last_name', $lastname);
-					$stmt->bindParam(':isAdmin', $is_admin);
-
-					$is_admin = $_POST["admin"];
-					if(isset($_POST["admin"])){
-						$is_admin = $_POST["admin"];
-					}
-					else
-						$is_admin = 0;
-					
-					$email = $_POST["email"];
-					$password = $_POST["password"];
-					$password = strtoupper(hash('whirlpool', $password));
-					$firstname = $_POST["firstname"];
-					$lastname = $_POST["lastname"];
-
-					$stmt->execute();
-
-					echo '<script type="text/javascript">alert("Account was succesfully created!")</script>';
-					echo "<script>location.href = 'login.php'</script>";
+					echo '<script type="text/javascript">alert("E-mail must be valid and must have at least 5 characters.  The password must contain at least 5 characters.")</script>';
+					echo "<script>location.href = 'register.php'</script>";
 				}
 				else
 				{
-					echo '<script type="text/javascript">alert("An account with this email already exists")</script>';
-					echo "<script>location.href = 'register.php'</script>";
+					
+					$stmt = $dbh->prepare("SELECT `email` FROM `users` WHERE `email`=:email");
+					$stmt->bindParam(':email', $email);
+					$email = $_POST["email"];
+					$stmt->execute();
+					$count = $stmt->rowCount();
+					if ($count == 0)
+					{
+						$stmt =  $dbh->prepare("INSERT INTO `users` (email, password, first_name, last_name, isAdmin) VALUES (:email, :password, :first_name, :last_name, :isAdmin)");
+
+						$stmt->bindParam(':email', $email);
+						$stmt->bindParam(':password', $password);
+						$stmt->bindParam(':first_name', $firstname);
+						$stmt->bindParam(':last_name', $lastname);
+						$stmt->bindParam(':isAdmin', $is_admin);
+
+						$is_admin = $_POST["admin"];
+						if(isset($_POST["admin"])){
+							$is_admin = $_POST["admin"];
+						}
+						else
+							$is_admin = 0;
+						
+						$email = $_POST["email"];
+						$password = $_POST["password"];
+						$password = strtoupper(hash('whirlpool', $password));
+						$firstname = $_POST["firstname"];
+						$lastname = $_POST["lastname"];
+
+						$stmt->execute();
+
+						echo '<script type="text/javascript">alert("Account was succesfully created!")</script>';
+						echo "<script>location.href = 'login.php'</script>";
+					}
+					else
+					{
+						echo '<script type="text/javascript">alert("An account with this email already exists")</script>';
+						echo "<script>location.href = 'register.php'</script>";
+					}
 				}
 				$dbh = null;
 			}
+			else{
+				echo '<script type="text/javascript">alert("Passwords must match.")</script>';
+				echo "<script>location.href = 'register.php'</script>";
+			}
 		}
-		else{
-			echo '<script type="text/javascript">alert("Passwords must match.")</script>';
-			echo "<script>location.href = 'register.php'</script>";
-		}
+
 	}
   ?>
